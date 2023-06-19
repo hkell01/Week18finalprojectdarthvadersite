44a07a19f10b67c2bf5d5690bc00cdb216782e55
Hayden Kelley <haydenkelley11@gmail.com>
2023-06-19 13:54:59 -0500

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>This is the footer section for Darth Vader, a tragic story.</p>
+    </footer>
+  );
+};
+
+export default Footer;
